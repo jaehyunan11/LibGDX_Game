7e12cd81a663039d8c7fc229d5f26702a75f65c8
jaehyun <jaehyuna11@gmail.com>
2021-12-11 23:10:26 -0800

    conditons

--- MyTester/src/Conditions.java
+++ MyTester/src/Conditions.java
@@ -1,16 +1,47 @@
 public class Conditions {
     public static void main (String[] args) {
-        int speed = 11;
+        int speed = 0;
 
-        if (speed > 11) {
-//            The condition above needs to be true so that we execute the code
-            System.out.println("Speed is greater");
+        switch (speed) {
+            case 1:
+//                execute the code that is here
+                System.out.println("Speed is 1");
+                break;
 
-        } else if (speed == 11) {
-            System.out.println("Speed is Equal");
-        } else {
-            System.out.println("Speed is lower");
+            case 2:
+                System.out.println("Speed is 2");
+                break;
+
+            case 3:
+                System.out.println("Speed is 3");
+                break;
+
+            default:
+                System.out.println("Default case executed");
+                break;
         }
+//        After break, we go here
+
+        if (speed == 1) {
+
+        } else if (speed == 2) {
+
+        } else if (speed == 3) {
+
+        } else {
+//           if nothign above is ture, then execute the code here
+        }
+
+
+//        if (speed > 11) {
+////            The condition above needs to be true so that we execute the code
+//            System.out.println("Speed is greater");
+//
+//        } else if (speed == 11) {
+//            System.out.println("Speed is Equal");
+//        } else {
+//            System.out.println("Speed is lower");
+//        }
 
     }
 
