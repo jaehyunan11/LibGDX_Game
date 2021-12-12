9dbfb2dfd6707c06a561746adcd94184f6a2d2b6
jaehyun <jaehyuna11@gmail.com>
2021-12-11 23:27:26 -0800

    Loops

---
+++ MyTester/src/Loops.java
@@ -0,0 +1,19 @@
+public class Loops {
+
+    public static void main(String[] args) {
+//        for int i which is equal to zero, as long as i is less then 10
+//        loop, then increment i by 1
+
+//        for(int i=0; i <= 10; i++) {
+//            System.out.println("Printed " + i + " times");
+//        }
+
+        int i = 0;
+//        while i is less then 10 execute whats in the while loop
+        while(i <= 10) {
+//            The condition needs to be true in order to execute the code
+            System.out.println("Printed " + i + " times");
+            i++;
+        }
+    }
+}
